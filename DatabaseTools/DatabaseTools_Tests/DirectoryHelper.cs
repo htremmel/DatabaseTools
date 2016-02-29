f777118220d924e058ad1c3908ea9fafa49556c3
htremmel <hans.tremmel@gmail.com>
2016-02-28 21:16:26 -0800

    Added files via upload

---
+++ DatabaseTools/DatabaseTools_Tests/DirectoryHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DatabaseTools_Tests
+{
+    class DirectoryHelper
+    {
+        FileInfo _fi;
+        List<string> _paths;
+        int _lim = 5; //Limits the number of saves to keep it light weight
+
+        #region Properties
+        public List<string> SavedPaths
+        {
+            get
+            {
+                return _paths;
+            }
+        }
+
+        public int PathLimit
+        {
+            set
+            {
+                _lim = value;
+            }
+            get
+            {
+                return _lim;
+            }
+        }   
+        #endregion        
+
+        #region Constructors
+        public DirectoryHelper(FileInfo fi)
+        {
+            
+        }
+
+        public DirectoryHelper(string path)
+        {
+
+        }
+        #endregion
+        
+
+        public void ParentDirectory()
+        {
+                
+        }
+
+        public void RestorePrevious()
+        {
+
+        }
+    }
+}
